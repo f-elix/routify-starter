ac56012922bd9a6dc733e06559aba0707a0cf5ad
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-16 10:51:55 +0100

    test

--- scripts/now/build.js
+++ scripts/now/build.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra')
 const { execSync } = require('child_process');
 
+console.log(fs.readdirSync('..'))
 if (!fs.pathExistsSync('../../dist')) {
     console.log('Building app...')
     execSync('npm run build:app', {stdio: 'inherit'})
